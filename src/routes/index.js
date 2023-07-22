5652eca519f21963ddc7064226e08cdcc54d09a8
A-SED-HASAN <hasanaghaei6@gmail.com>
2023-07-23 01:47:50 +0330

    Delete src/routes directory

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import Error from './global/Error'
-import SignIn from './global/SignIn'
-import PrivateRoute from './global/PrivateRoute'
-
-import Budget from './pages/Budget'
-import Insights from './pages/Insights'
-import Goals from './pages/Goals'
-
-export { SignIn, Error, Budget, PrivateRoute, Insights, Goals }
